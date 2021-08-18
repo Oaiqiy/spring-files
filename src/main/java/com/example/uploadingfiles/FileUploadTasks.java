9d75c9cce60830197e9f53c2f4e21b1a8d304dd4
pyiqiao <1320371940@qq.com>
2021-08-18 15:04:40 +0800

    修复定时任务bug

--- src/main/java/com/example/uploadingfiles/FileUploadTasks.java
+++ src/main/java/com/example/uploadingfiles/FileUploadTasks.java
@@ -1,11 +1,13 @@
 package com.example.uploadingfiles;
 
 import com.example.uploadingfiles.storage.StorageService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class FileUploadTasks {
 
     StorageService storageService;
@@ -15,8 +17,11 @@ public class FileUploadTasks {
         this.storageService = storageService;
     }
 
-    @Scheduled(cron = "* 0,30 * * * *")
+    @Scheduled(cron = "0 0,30 * * * *")
     void autoDeleteAll(){
         storageService.deleteAll();
+        storageService.init();
+        log.info("delete all");
+
     }
 }
